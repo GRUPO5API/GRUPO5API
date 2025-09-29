e346793baa2aa2ccc9dc1e8bfacfd4f07aaed8c0
Samuel Zarate <a2024110835@estudiantes.upsa.edu.bo>
2025-09-29 19:45:01 -0400

    Nuget, Program.cs

--- PrimerParcialAPI/obj/Debug/net9.0/PrimerParcialAPI.AssemblyInfo.cs
+++ PrimerParcialAPI/obj/Debug/net9.0/PrimerParcialAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerParcialAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3036598cf8ec2726f4aaf7d0f4ec3a4156e0a0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerParcialAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerParcialAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
